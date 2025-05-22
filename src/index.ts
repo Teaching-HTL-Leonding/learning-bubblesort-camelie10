914155519c3c6ff79f7f793f00868241ef655a31
camelie10 <amelietingtingchen@gmail.com>
2025-05-22 10:29:07 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+const numbers = [7, 1, 9, 5, 3, 2, 6, 4, 8];
+sortNum(numbers);
+console.log(numbers);
+
+const names = ['tom', 'bruce', 'adam', 'zoe','clara'];
+sortNames(names);
+console.log(names);
+
+function sortNum(numbers: number[]){
+  let swapped: boolean;
+  let numberofSortedElements = 0;
+  do {
+    swapped = false;
+    for (let i = 0; i < numbers.length - 1 - numberofSortedElements; i++) {
+      const j = i + 1;
+      if (numbers[j] < numbers[i]) {
+        const temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+        swapped = true;
+      }
+    }
+    numberofSortedElements++;
+  } while (swapped);
+}
+
+function sortNames(names: string[]){
+  let swapped: boolean;
+  let numberofSortedElements = 0;
+  do {
+    swapped = false;
+    for (let i = 0; i < names.length - 1 - numberofSortedElements; i++) {
+      const j = i + 1;
+      if (names[j] < names[i]) {
+        const temp = names[i];
+        names[i] = names[j];
+        names[j] = temp;
+        swapped = true;
+      }
+    }
+    numberofSortedElements++;
+  } while (swapped);
+}
